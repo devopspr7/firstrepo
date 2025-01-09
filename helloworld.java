99cae9b1b54707bb1a443a0facbe9f1471463296
devopspr <devopspr7@gmail.com>
2025-01-09 13:57:44 +0530

    new files added

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 //My first program
-class Helloworld {
-    public static void main(String[] args){
-        system.out.println("Hello,World!!!!");
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
     }
 }
